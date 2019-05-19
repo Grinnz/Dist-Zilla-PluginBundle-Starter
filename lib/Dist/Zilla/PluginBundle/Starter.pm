3bb293ab351d445d1530a8192ee94bf3044a9c5f
Dan Book <grinnz@grinnz.com>
2019-05-19 12:12:22 -0400

    Bump version

--- lib/Dist/Zilla/PluginBundle/Starter.pm
+++ lib/Dist/Zilla/PluginBundle/Starter.pm
@@ -6,7 +6,7 @@ with 'Dist::Zilla::Role::PluginBundle::Easy',
   'Dist::Zilla::Role::PluginBundle::PluginRemover';
 use namespace::clean;
 
-our $VERSION = 'v3.0.3';
+our $VERSION = 'v3.0.4';
 
 # Revisions can include entries with the standard plugin name, array ref of plugin/name/config,
 # or coderefs which are passed the pluginbundle object and return a list of plugins in one of these formats.
